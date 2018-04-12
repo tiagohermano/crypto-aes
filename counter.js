a51c316c22641d1a7ed2e62a920f0e0110916b4e
Tiago Hermano <tiagofhermano@gmail.com>
2018-04-12 09:09:37 -0300

    Comentado uso da biblioteca 'aes-js' e leitura de texto em arquivo e sua descriptografia

--- counter.js
+++ counter.js
@@ -1,32 +1,29 @@
 var aesjs = require('aes-js');
 
-module.exports = function ctr(text) {
-  // An example 128-bit key (16 bytes * 8 bits/byte = 128 bits)
+module.exports = function counter(text) {
+  // Exemplo de chave: chave de 128-bit (16 bytes * 8 bits/byte = 128 bits)
   var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
 
-  // Convert text to bytes
+  // Conversão de texto para bytes
   var textBytes = aesjs.utils.utf8.toBytes(text);
 
-  // The counter is optional, and if omitted will begin at 1
+  // Counter é opcional, e se omitido começará em 1
   var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
   var encryptedBytes = aesCtr.encrypt(textBytes);
 
-  // To print or store the binary data, you may convert it to hex
+  // Conversão de bytes para hex
   var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
   console.log('Hex criptografado: ',encryptedHex);
-  // "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
-  //  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
 
-  // When ready to decrypt the hex string, convert it back to bytes
+  // DECRYPT
+
+  // Conversão da string em hex para bytes novamente
   var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
 
-  // The counter mode of operation maintains internal state, so to
-  // decrypt a new instance must be instantiated.
   var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
   var decryptedBytes = aesCtr.decrypt(encryptedBytes);
 
-  // Convert our bytes back into text
+  // Conversão dos bytes em texto
   var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
   console.log('Texto descriptografado: ',decryptedText);
-  // "Text may be any length you wish, no padding is required."
 }
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var aesjs = require('aes-js');
 var fs = require('fs');
-var ctr = require('./ctr');
+var path = require('path');
+var ctr = require('./counter');
 
-var texto = 'O texto pode ser de qualquer tamanho';
-ctr(texto);
+var filePath = path.join(__dirname, "texto.txt");
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err,texto){
+    if (!err) {
+        console.log("texto lido: ",texto);
 
-// var filePath = path.join(__dirname, "texto.txt");
-// fs.readFile(filePath, {encoding: 'utf-8'}, function(err,texto){
-//     if (!err) {
-//         console.log("texto lido: ",texto);
-//
-//         var decriptografado = decriptaArq(texto);
-//         console.log("texto decriptografado: ",decriptografado);
-//     }
-// });
+        counter(texto);
+    }
+});
